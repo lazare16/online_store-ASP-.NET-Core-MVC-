6050875c742922543db699fe914d7a60f937798c
lazare16 <gogoladzelazare@gmail.com>
2026-01-17 17:09:21 +0400

    Addition: adding initial files

---
+++ online store MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace online_store_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
